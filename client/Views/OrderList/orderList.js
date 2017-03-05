230238a7d09342754b2d5bc3649b1541b100a876
Jeremy Haberman <jeremy@thehabermans.com>
2017-03-05 12:04:30 -0600

    Change indents in orderList.js source

--- client/Views/OrderList/orderList.js
+++ client/Views/OrderList/orderList.js
@@ -1,17 +1,17 @@
 Template.orderList.helpers({
-   orders: function() {
-       return Orders.find({}, {sort: {createdDateTime: -1}});
-   },
-    sum : function() {
-        var allOrders = Orders.find().fetch();
-        var sum = 0;
-        allOrders.forEach(function(order) {
-          if(!order.deleted) {
-            sum += order.grandTotal;
-          }
-        });
-        return sum;
-    }
+  orders: function() {
+     return Orders.find({}, {sort: {createdDateTime: -1}});
+  },
+  sum : function() {
+      var allOrders = Orders.find().fetch();
+      var sum = 0;
+      allOrders.forEach(function(order) {
+        if(!order.deleted) {
+          sum += order.grandTotal;
+        }
+      });
+      return sum;
+  }
 
 });
 
